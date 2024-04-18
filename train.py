0afc3e5099e3971d0f424640a092f77da5ed620d
aakashv100 <vardhan.aakash1@gmail.com>
2024-04-18 11:30:30 +0400

    Refactor device selection in train.py

--- train.py
+++ train.py
@@ -409,7 +409,7 @@ if __name__ == '__main__':
     opt.weights = last if opt.resume else opt.weights
     print(opt)
     opt.img_size.extend([opt.img_size[-1]] * (3 - len(opt.img_size)))  # extend to 3 sizes (min, max, test)
-    device = torch_utils.select_device(opt.device, apex=mixed_precision, batch_size=opt.batch_size)
+    device = torch_utils.select_device(opt.device, batch_size=opt.batch_size)
     if device.type == 'cpu':
         mixed_precision = False
 
